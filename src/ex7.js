f2c1898ff08d9356759ccbbc581edac1b1f4264c
Prayog Paudel <ppaudel93@gmail.com>
2018-12-08 21:17:55 +0545

    ex7 started

---
+++ src/ex7.js
@@ -0,0 +1,13 @@
+// 7. Write a JavaScript program to find 1st January is being a Sunday between 2014 and 2050.
+var weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+for (i = 2014; i <= 2050; i++) {
+  let loopdate = new Date(i, 0, 1);
+}
